ab58a8bc9774afa615c3c8893c38aff97da4826d
artpaul <artmkin@gmail.com>
2017-03-06 17:19:01 +0500

    add define _unix_

--- clickhouse/base/platform.h
+++ clickhouse/base/platform.h
@@ -1,18 +1,22 @@
 #pragma once
 
 #if defined(__linux__)
-#	define _linux_
+#   define _linux_
 #elif defined(_WIN64)
-#	define _win64_
-#	define _win32_
+#   define _win64_
+#   define _win32_
 #elif defined(__WIN32__) || defined(_WIN32)
-#	define _win32_
+#   define _win32_
 #elif defined(__APPLE__)
-#	define _darwin_
+#   define _darwin_
 #endif
 
 #if defined(_win32_) || defined(_win64_)
-#	define _win_
+#   define _win_
+#endif
+
+#if defined(_linux_)
+#   define _unix_
 #endif
 
 #if defined(_MSC_VER)
