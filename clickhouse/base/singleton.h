34c65b8071df8288f657397bbe888aa0afa52df0
artpaul <artmkin@gmail.com>
2017-03-16 23:59:57 +0500

    simplify singleton

--- clickhouse/base/singleton.h
+++ clickhouse/base/singleton.h
@@ -1,22 +1,11 @@
 #pragma once
 
-#include <memory>
-#include <mutex>
-
 namespace clickhouse {
 
 template <typename T>
 T* Singleton() {
-    static std::once_flag flag;
-    static std::unique_ptr<T> instance;
-
-    std::call_once(flag, []
-        {
-            instance.reset(new T);
-        }
-    );
-
-    return instance.get();
+    static T instance;
+    return &instance;
 }
 
 }
