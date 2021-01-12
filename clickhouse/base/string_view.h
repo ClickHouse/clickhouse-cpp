822f91148d0b5549206825e8e90c44acc2a088ed
Mike Kot <to@myrrc.dev>
2021-01-12 18:35:23 +0300

    fix

--- clickhouse/base/string_view.h
+++ clickhouse/base/string_view.h
@@ -4,22 +4,28 @@
 #include <stdexcept>
 #include <string>
 
-#if defined(__APPLE__)
-    #include <string_view>
+#if defined(__APPLE__) // AppleClang
+#   include <string_view>
     using string_view = std::string_view;
     using std::string_view_literals::operator""sv;
 #elif defined(__GNUC__) && __GNUC__ < 7
-    #include <experimental/string_view>
+#   include <experimental/string_view>
     using string_view = std::experimental::string_view;
-
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wliteral-suffix"
+#   if defined(__llvm__) || defined(__clang__) // linux clang 6.0
         using std::experimental::string_view_literals::operator""sv;
-    #pragma GCC diagnostic pop
+#   else // gcc 6.
+#       pragma GCC diagnostic push
+#       pragma GCC diagnostic ignored "-Wliteral-suffix"
+            using std::experimental::string_view_literals::operator""sv;
+#       pragma GCC diagnostic pop
+#   endif
 #else
-    #include <string_view>
+#   include <string_view>
     using string_view = std::string_view;
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wliteral-suffix" // gcc 7
     using std::literals::string_view_literals::operator""sv;
+#   pragma GCC diagnostic pop
 #endif
 
 /**
