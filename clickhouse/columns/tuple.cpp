70034a23bf272c3729b3dd7ff69786a606543419
Vasily Nemkov <V.Nemkov@gmail.com>
2022-01-18 19:07:38 +0300

    Minor optimization and style fixes

--- clickhouse/columns/tuple.cpp
+++ clickhouse/columns/tuple.cpp
@@ -26,20 +26,22 @@ void ColumnTuple::Append(ColumnRef column) {
             "can't append column of type " + column->Type()->GetName() + " "
             "to column type " + this->Type()->GetName());
     }
-    for (size_t ci = 0; ci < columns_.size(); ci++) {
-        columns_[ci]->Append((*column->As<ColumnTuple>())[ci]);
+    const auto & source_tuple_column = column->As<ColumnTuple>();
+    for (size_t ci = 0; ci < columns_.size(); ++ci) {
+        columns_[ci]->Append((*source_tuple_column)[ci]);
     }
 }
 size_t ColumnTuple::Size() const {
     return columns_.empty() ? 0 : columns_[0]->Size();
 }
 ColumnRef ColumnTuple::Slice(size_t begin, size_t len) const {
-    std::vector<ColumnRef> slicedColumns;
+    std::vector<ColumnRef> sliced_columns;
+    sliced_columns.reserve(columns_.size());
     for(const auto &column : columns_){
-        slicedColumns.push_back(column->Slice(begin, len));
+        sliced_columns.push_back(column->Slice(begin, len));
     }
 
-    return std::make_shared<ColumnTuple>(slicedColumns);
+    return std::make_shared<ColumnTuple>(sliced_columns);
 }
 
 bool ColumnTuple::Load(InputStream* input, size_t rows) {
