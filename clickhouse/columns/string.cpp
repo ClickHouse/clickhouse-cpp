e76cb292271875799a0dd4de6d9ef71077bac203
wangwei <1261385937@qq.com>
2022-11-19 13:44:48 +0800

    remove items_.reserve for batch write

--- clickhouse/columns/string.cpp
+++ clickhouse/columns/string.cpp
@@ -247,7 +247,6 @@ void ColumnString::Append(ColumnRef column) {
         // TODO: fill up existing block with some items and then add a new one for the rest of items
         if (blocks_.size() == 0 || blocks_.back().GetAvailable() < total_size)
             blocks_.emplace_back(std::max(DEFAULT_BLOCK_SIZE, total_size));
-        items_.reserve(items_.size() + col->Size());
 
         for (size_t i = 0; i < column->Size(); ++i) {
             this->AppendUnsafe((*col)[i]);
