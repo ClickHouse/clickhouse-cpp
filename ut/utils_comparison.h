5aab78e529f6f9dc4285617ddac3c5e003199896
Vasily Nemkov <V.Nemkov@gmail.com>
2025-06-16 16:05:24 +0200

    Modernized and fixed Linux CI/CD jobs
    - Using Ubuntu 22.04 and Ubuntu 24.04
    - corresponding supported compilers
    - Properly installing docker
    - CompareRecursive fix for GCC-12 (fixes #425)

--- ut/utils_comparison.h
+++ ut/utils_comparison.h
@@ -141,11 +141,15 @@ template <typename LeftContainer, typename RightContainer>
 template <typename Container>
 struct PrintContainer;
 
+
 template <typename Left, typename Right>
 ::testing::AssertionResult CompareRecursive(const Left & left, const Right & right) {
-    if constexpr (!is_string_v<Left> && !is_string_v<Right>
-            && (is_container_v<Left> || std::is_base_of_v<clickhouse::Column, std::decay_t<Left>>)
-            && (is_container_v<Right> || std::is_base_of_v<clickhouse::Column, std::decay_t<Right>>) ) {
+    using L = std::decay_t<Left>;
+    using R = std::decay_t<Right>;
+
+    if constexpr (!is_string_v<L> && !is_string_v<R>
+            && (is_container_v<L> || std::is_base_of_v<clickhouse::Column, L>)
+            && (is_container_v<R> || std::is_base_of_v<clickhouse::Column, R>) ) {
 
         const auto & l = maybeWrapColumnAsContainer(left);
         const auto & r = maybeWrapColumnAsContainer(right);
@@ -155,24 +159,29 @@ template <typename Left, typename Right>
         else
             return result << "\nExpected container: " << PrintContainer{l}
                           << "\nActual container  : " << PrintContainer{r};
+    }
+    else if constexpr (std::is_same_v<char const *, L> || std::is_same_v<char *, L>) {
+        return CompareRecursive(std::string_view(left), right);
+    }
+    else if constexpr (std::is_same_v<char const *, R> || std::is_same_v<char *, R>) {
+        return CompareRecursive(left, std::string_view(right));
     } else {
         if (left != right) {
-
             // Handle std::optional<float>(nan)
-            // I'm too lazy to code comparison against std::nullopt, but this shpudn't be a problem in real life.
-            // RN comparing against std::nullopt, you'll receive an compilation error.
-            if constexpr (is_instantiation_of<std::optional, Left>::value && is_instantiation_of<std::optional, Right>::value)
+            // I'm too lazy to code comparison against std::nullopt, but this shouldn't be a problem in real life.
+            // RN comparing against std::nullopt, you'll get a compilation error.
+            if constexpr (is_instantiation_of<std::optional, L>::value && is_instantiation_of<std::optional, R>::value)
             {
                 if (left.has_value() && right.has_value())
                     return CompareRecursive(*left, *right);
             }
-            else if constexpr (is_instantiation_of<std::optional, Left>::value) {
+            else if constexpr (is_instantiation_of<std::optional, L>::value) {
                 if (left)
                     return CompareRecursive(*left, right);
-            } else if constexpr (is_instantiation_of<std::optional, Right>::value) {
+            } else if constexpr (is_instantiation_of<std::optional, R>::value) {
                 if (right)
                     return CompareRecursive(left, *right);
-            } else if constexpr (std::is_floating_point_v<Left> && std::is_floating_point_v<Right>) {
+            } else if constexpr (std::is_floating_point_v<L> && std::is_floating_point_v<R>) {
                 if (std::isnan(left) && std::isnan(right))
                     return ::testing::AssertionSuccess();
             }
