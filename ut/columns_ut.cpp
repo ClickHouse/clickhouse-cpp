7dc75acd33e9a2b183b5e2b628ef8f09b341945b
Minh Vu <vuhoangminh97@gmail.com>
2026-08-24 15:23:49 +0200

    Test Decimal precision mismatch with same storage type

--- ut/columns_ut.cpp
+++ ut/columns_ut.cpp
@@ -224,6 +224,20 @@ TEST(ColumnsCase, DecimalSwapDifferentPrecision) {
     EXPECT_EQ(column2->StringAt(0), "4.56");
 }
 
+TEST(ColumnsCase, DecimalSwapDifferentPrecisionSameStorageType) {
+    auto column1 = std::make_shared<ColumnDecimal>(17, 2);
+    auto column2 = std::make_shared<ColumnDecimal>(18, 2);
+    column1->Append("1.23");
+    column2->Append("4.56");
+
+    EXPECT_THROW(column1->Swap(*column2), ValidationError);
+
+    EXPECT_EQ(column1->GetType().GetName(), "Decimal(17,2)");
+    EXPECT_EQ(column2->GetType().GetName(), "Decimal(18,2)");
+    EXPECT_EQ(column1->StringAt(0), "1.23");
+    EXPECT_EQ(column2->StringAt(0), "4.56");
+}
+
 TEST(ColumnsCase, NumericInit) {
     auto col = std::make_shared<ColumnUInt32>(MakeNumbers());
 
